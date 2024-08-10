f549f4bea29fcc8e449b9230843398168b4e898f
Junaid-Khan <80524662+JunaidKhan44@users.noreply.github.com>
2024-08-10 11:42:49 +0500

    Source Code.
    Source Code.

---
+++ spotless eg/spotless-eg/src/test/java/com/jk/spotless/SpotlessExampleApplicationTests.java
@@ -0,0 +1,11 @@
+package com.jk.spotless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotlessExampleApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
