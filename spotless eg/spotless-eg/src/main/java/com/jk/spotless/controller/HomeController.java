7132a5dda057a5a7ce23ce9b7eb86435ea334092
Junaid-Khan <80524662+JunaidKhan44@users.noreply.github.com>
2024-08-20 18:57:03 +0500

    Update HomeController.java

--- spotless eg/spotless-eg/src/main/java/com/jk/spotless/controller/HomeController.java
+++ spotless eg/spotless-eg/src/main/java/com/jk/spotless/controller/HomeController.java
@@ -3,6 +3,12 @@ package com.jk.spotless.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ *
+ * @author Junaid.Khan
+ *
+**/    
+
 @RestController
 public class HomeController {
 
